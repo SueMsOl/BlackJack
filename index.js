108e811293d47110bfcc379d62b6c5fe4a4534bd
SueMsOl <sogandmsol17@gmail.com>
2025-03-23 23:10:59 +0000

    sum bug resolved

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-//+needs modify: 1 has 11 score. >10 has 10 score. make changes
+//+needs modify: 
 //+a new place to showcase all the money left after loss and wins
 
 
 let x = [] //array of cards. it is universal. so any reassignment or modification is saved here.
+let scores = [] //number 1 has score of 11. >10 have score of 10.
 let sum = 0  //to show sum of cards
 let betAmount = 0
 let hasBlackJack = false //initially, false, if 21, it changes
@@ -12,6 +13,7 @@ let player = {
     title : 'Your chips',
    // chips: 455  
 }
+console.log(scores)
 
 let playerEl = document.getElementById('player-el')
 playerEl.textContent = `${player.title}: `  //initial bet screen
@@ -26,6 +28,19 @@ function placeBet(){
     betAmount = +(betEl.value)
     playerEl.textContent += `${betAmount}$` //parseInt() or Number()
 }
+function getSum(){
+    for (let i = 0; i < x.length; i++){
+        if (x[i] === 1){
+            scores.push(11)
+        } else if(x[i] >= 10){
+            scores.push(10)
+        }else{
+            scores.push(x[i])
+        }
+    }
+    sum = scores.reduce((a,b) => a+b,0);
+    return sum;
+}
 //makes a random number between 1 to 13(incl):
 function getRandomInt(){   //to get a random cards number
    return Math.floor(Math.random()*(13-1)+1);
@@ -35,13 +50,15 @@ function startGame() {
     placeBet()      
     isAlive = true
     x = [getRandomInt(),getRandomInt()]  //x array is reassigned. from [] to having 2 numbers
+    scores = []
     renderGame()
 }
 //runs when new game is clicked:
 function newCard(){   
     if (isAlive===true && hasBlackJack===false){
         let card = getRandomInt() //get a random card
-        x.push(card); //x array modified. from 2 numbers to 3 numbers 
+        x.push(card); //x array modified. from 2 numbers to 3 numbers
+        scores = [] 
         renderGame()
     }
     else if (isAlive===false){
@@ -54,8 +71,6 @@ function newCard(){
 
 //core function
 function renderGame() {
-
-   let sum = x.reduce((a,b) => a+b,0);  //sum x array's items
 // cards initial text:
     cardsEl.textContent = `Cards: ` 
 //cards' text updates with x:
@@ -63,19 +78,21 @@ function renderGame() {
         cardsEl.textContent += `${x[i]} `
     }
 //sum text  
-   sumEl.textContent = `Sum: ${sum}` 
+
+   sumEl.textContent = `Sum: ` 
+   sumEl.textContent += `${getSum()}`;
 //message text  
    if (sum <= 20) {
        message = "Do you want to draw a new card?"
    } else if (sum === 21) {
        message = "You've got Blackjack!"
        hasBlackJack = true
-       player.chips += player.chips 
+       //player.chips += player.chips 
        playerEl.textContent = `${player.title}: ${betAmount*2}$ `
    } else {
        message = "You're out of the game!"
        isAlive = false
-       player.chips -= player.chips 
+       //player.chips -= player.chips 
        playerEl.textContent = `${player.title}: ${betAmount-betAmount}$ `
    }
    messageEl.textContent = message
